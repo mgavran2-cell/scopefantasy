a519d3f6247385d29dddda62c262ebe2ddbc6d29
Marko Gavran <mgavran2@net.efzg.hr>
2026-03-24 21:39:09 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Toaster } from "@/components/ui/toaster"
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
